7199ae94deefb8f83e2dd6c5e57a9de644c6ba70
Sriram Senthil <sriram.senthil153@gmail.com>
2024-02-06 19:11:58 -0500

    Tested token refill

--- TokenBucket.java
+++ TokenBucket.java
@@ -3,27 +3,30 @@ package RateLimiter;
 public class TokenBucket{
     private final long maxTokens;
     private final long refillRate;
-    private final long refillAmount;
+   // private final long refillAmount;
     private long availableTokens;
     private long lastRefill;
+    long maxRefillElapsed = 60 * 1_000_000_000L; // 60 seconds in nanoseconds
 
-    public TokenBucket(long maxTokens, long refillRate, long refillAmount){
+    public TokenBucket(long maxTokens, long refillRate){
         this.maxTokens = maxTokens;
         this.refillRate = refillRate;
-        this.refillAmount = refillAmount;
+        //this.refillAmount = refillAmount;
         this.availableTokens = maxTokens;
         this.lastRefill = System.nanoTime();
-
+        this.refill();
     }
 
     private void refill(){
         long currTime = System.nanoTime();
-        long refillRateNano = 1_000_000_000 / refillRate;
-        long elapsed = (currTime - lastRefill) / refillRateNano;
+        long elapsed = currTime - lastRefill;
+        
+        elapsed = Math.min(elapsed, maxRefillElapsed);
 
-        if(elapsed>0){
-            availableTokens = Math.min(maxTokens, availableTokens+(elapsed*refillAmount));
-            lastRefill = currTime;
+        long refillTokens = (elapsed * refillRate) / 1_000_000_000L; // Convert elapsed time back to seconds for refill calculation
+        if (refillTokens > 0) {
+            availableTokens = Math.min(maxTokens, availableTokens + refillTokens);
+            lastRefill += elapsed; 
         }
     }
 
@@ -36,5 +39,30 @@ public class TokenBucket{
         } else {
             return false;
         }
+    }
+
+    public synchronized long getAvailableTokens() {
+        refill();
+        return availableTokens;
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        TokenBucket bucket = new TokenBucket(7, 1); // Small bucket for testing
+        // int allowedRequests = 0;
+        // for (int i = 0; i < 10; i++) {
+        //     if (bucket.checkRequest()) {
+        //         allowedRequests++;
+        //     }
+        // }
+        // System.out.println(allowedRequests);
+
+        bucket.checkRequest(); // Consumes 1 token, assuming this request is allowed.
+        long tokensBeforeWait = bucket.getAvailableTokens();
+        System.out.println("Available tokens before wait: " + tokensBeforeWait);
+
+        Thread.sleep(5000);
+        long tokensAfterWait = bucket.getAvailableTokens();
+        System.out.println("Available tokens after wait: " + tokensAfterWait);
+
     }
 }
